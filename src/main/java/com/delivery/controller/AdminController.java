a901fa810819d747d65a41a26ecb15967e609768
Nikita Tolstykh <nikitatolstyh7@gmail.com>
2025-06-22 19:21:49 +0200

    Amin controller methods GET for vehicles

--- src/main/java/com/delivery/controller/AdminController.java
+++ src/main/java/com/delivery/controller/AdminController.java
@@ -1,11 +1,32 @@
 package com.delivery.controller;
 
+import com.delivery.dto.VehicleDto;
+import com.delivery.service.VehicleService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin")
 public class AdminController {
+    private final VehicleService vehicleService;
 
+    public AdminController(VehicleService vehicleService) {
+        this.vehicleService = vehicleService;
+    }
+
+    @GetMapping("/vehicles")
+    public ResponseEntity<List<VehicleDto>> getAllVehicles() {
+        return ResponseEntity.ok(vehicleService.getAllVehicles());
+    }
+
+    @GetMapping("/vehicles/{id}")
+    public ResponseEntity<VehicleDto> getVehicle(@PathVariable Long id) {
+        return ResponseEntity.ok(vehicleService.getVehicle(id));
+    }
 
 }
