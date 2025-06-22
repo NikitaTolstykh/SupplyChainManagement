e83f0627edb8ebd7f470fd976dd134d8b3f36377
Nikita Tolstykh <nikitatolstyh7@gmail.com>
2025-06-22 22:16:09 +0200

    AdminService

--- src/main/java/com/delivery/service/AdminService.java
+++ src/main/java/com/delivery/service/AdminService.java
@@ -1,4 +1,14 @@
 package com.delivery.service;
 
+import com.delivery.dto.UserDto;
+
+import java.util.List;
+
 public interface AdminService {
+    List<UserDto> getAllWorkers();
+    UserDto getWorker(Long id);
+    UserDto addWorker(UserDto userDto);
+    UserDto editWorker(Long id, UserDto userDto);
+    void deleteWorker(Long id);
+
 }
