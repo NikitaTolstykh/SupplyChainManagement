acbaad5e25546d43a5c257177c2b9e73cd17c42c
Nikita Tolstykh <nikitatolstyh7@gmail.com>
2025-06-28 23:40:35 +0200

    Created DTO for:info about available drivers and updating order status

---
+++ src/main/java/com/delivery/dto/AvailableDriverDto.java
@@ -0,0 +1,12 @@
+package com.delivery.dto;
+
+public class AvailableDriverDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String email;
+    private String vehicleBrand;
+    private String vehicleModel;
+    private String licensePlate;
+}
