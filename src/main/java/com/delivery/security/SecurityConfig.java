4bf27307cd9c5dd686d3e989f2413d7cc6cc32f2
Nikita Tolstykh <nikitatolstyh7@gmail.com>
2025-06-29 19:02:54 +0200

    access for dispatcher api for dispatchers

--- src/main/java/com/delivery/security/SecurityConfig.java
+++ src/main/java/com/delivery/security/SecurityConfig.java
@@ -23,6 +23,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/admin/**").hasRole("ADMIN")
                         .requestMatchers("/api/client/**").hasRole("CLIENT")
+                        .requestMatchers("api/dispatcher/**").hasRole("DISPATCHER")
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
