cdcef50acbc52160aa40aff85679080349c270c2
Nikita Tolstykh <nikitatolstyh7@gmail.com>
2025-07-03 21:59:19 +0200

    Dto for client statistics

---
+++ src/main/java/com/delivery/dto/ClientStatisticsDto.java
@@ -0,0 +1,26 @@
+package com.delivery.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClientStatisticsDto {
+    private Long totalOrders;
+    private Long completedOrders;
+    private Long cancelledOrders;
+    private BigDecimal totalSpent;
+    private BigDecimal averageOrderValue;
+    private Map<String, Long> ordersByMonth;
+    private Map<String, BigDecimal> spentByMonth;
+    private double averageRating;
+    private String mostUsedRoute;
+
+}
