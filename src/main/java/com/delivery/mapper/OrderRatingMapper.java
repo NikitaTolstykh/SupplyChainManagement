9915053f5ef802c5ca54340042701bfb960c6970
Nikita Tolstykh <nikitatolstyh7@gmail.com>
2025-07-03 20:33:57 +0200

    api testers to gitignore and deleted dto for dispatcher list(I will use previous dto)

--- src/main/java/com/delivery/mapper/OrderRatingMapper.java
+++ src/main/java/com/delivery/mapper/OrderRatingMapper.java
@@ -1,13 +1,17 @@
 package com.delivery.mapper;
 
+import com.delivery.dto.DispatcherOrderRatingDto;
 import com.delivery.dto.OrderRatingResponseDto;
 import com.delivery.entity.OrderRating;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface OrderRatingMapper {
     @Mapping(source = "order.id", target = "orderId")
     @Mapping(expression = "java(orderRating.getClient().getFirstName() + \" \" + orderRating.getClient().getLastName())", target = "clientFullName")
     OrderRatingResponseDto toDto(OrderRating orderRating);
+
 }
