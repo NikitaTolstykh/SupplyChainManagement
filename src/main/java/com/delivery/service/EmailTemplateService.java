20a75d699ed35b36ecca0f89cd6de9bbc1dad0ec
Nikita Tolstykh <nikitatolstyh7@gmail.com>
2025-07-07 19:31:59 +0200

    method to get status message email

--- src/main/java/com/delivery/service/EmailTemplateService.java
+++ src/main/java/com/delivery/service/EmailTemplateService.java
@@ -3,6 +3,7 @@ package com.delivery.service;
 import com.delivery.entity.Order;
 import com.delivery.entity.User;
 import com.delivery.entity.Vehicle;
+import com.delivery.util.OrderStatus;
 
 public class EmailTemplateService {
 
@@ -47,28 +48,42 @@ public class EmailTemplateService {
 
     public String createOrderCreatedEmail(Order order) {
         return String.format("""
-            <html>
-            <body>
-                <h2>Order successfully created!!</h2>
-                <p>Hello, %s!</p>
-                <p>Your order #%d has been successfully created.</p>
-                <p><strong>Order details::</strong></p>
-                <ul>
-                    <li>From: %s</li>
-                    <li>To: %s</li>
-                    <li>Cargo type: %s</li>
-                    <li>Weight: %s кг</li>
-                    <li>Cost: %s руб.</li>
-                    <li>Pickup time: %s</li>
-                </ul>
-                <p>Order status: %s</p>
-                <p>We will notify you about the change in order status.</p>
-                <p>Best regards,<br>Delivery system team</p>
-            </body>
-            </html>
-            """, order.getClient().getFirstName(), order.getId(),
+                        <html>
+                        <body>
+                            <h2>Order successfully created!!</h2>
+                            <p>Hello, %s!</p>
+                            <p>Your order #%d has been successfully created.</p>
+                            <p><strong>Order details::</strong></p>
+                            <ul>
+                                <li>From: %s</li>
+                                <li>To: %s</li>
+                                <li>Cargo type: %s</li>
+                                <li>Weight: %s кг</li>
+                                <li>Cost: %s руб.</li>
+                                <li>Pickup time: %s</li>
+                            </ul>
+                            <p>Order status: %s</p>
+                            <p>We will notify you about the change in order status.</p>
+                            <p>Best regards,<br>Delivery system team</p>
+                        </body>
+                        </html>
+                        """, order.getClient().getFirstName(), order.getId(),
                 order.getFromAddress(), order.getToAddress(), order.getCargoType(),
                 order.getWeightKg(), order.getPrice(), order.getPickupTime(),
                 order.getStatus());
     }
+
+
+    private String getStatusMessage(OrderStatus status) {
+        return switch (status) {
+            case CREATED -> "Your order has been created and is awaiting processing.";
+            case ASSIGNED -> "The order has been assigned to the driver.";
+            case ACCEPTED -> "The driver has accepted the order";
+            case IN_PROGRESS -> "The driver started processing your order";
+            case DELIVERED -> "The order has been successfully delivered!";
+            case CANCELLED -> "The order has been cancelled.";
+        };
+    }
+
+
 }
