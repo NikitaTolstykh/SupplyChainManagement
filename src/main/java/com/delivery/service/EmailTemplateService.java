ee2d5d69c7d800a9f555818fcad33aaf5168ec63
Nikita Tolstykh <nikitatolstyh7@gmail.com>
2025-07-07 19:11:39 +0200

    method create vehicle assignment email

--- src/main/java/com/delivery/service/EmailTemplateService.java
+++ src/main/java/com/delivery/service/EmailTemplateService.java
@@ -1,6 +1,7 @@
 package com.delivery.service;
 
 import com.delivery.entity.User;
+import com.delivery.entity.Vehicle;
 
 public class EmailTemplateService {
 
@@ -22,5 +23,25 @@ public class EmailTemplateService {
                 """, worker.getFirstName(), worker.getEmail(), password);
     }
 
+    public String createVehicleAssignmentEmail(User driver, Vehicle vehicle) {
+        return String.format("""
+                        <html>
+                        <body>
+                            <h2>Vehicle assignment</h2>
+                            <p>Hello, %s!</p>
+                            <p>You have been assigned a vehicle:</p>
+                            <ul>
+                                <li>Brand: %s</li>
+                                <li>Model: %s</li>
+                                <li>Color: %s</li>
+                                <li>License plate: %s</li>
+                            </ul>
+                            <p>Treat your vehicle responsibly!</p>
+                            <p> Sincerely,<br>Delivery system team</p>
+                        </body>
+                        </html>
+                        """, driver.getFirstName(), vehicle.getBrand(), vehicle.getModel(),
+                vehicle.getLicensePlate(), vehicle.getColor());
+    }
 
 }
