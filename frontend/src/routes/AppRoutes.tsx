1b5a483732a94dc5e217ebca5ef7cdf06122aab6
Nikita Tolstykh <nikitatolstyh7@gmail.com>
2025-08-05 22:55:54 +0200

    feat: AppRoutes and ProtectedRoute

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from "../pages/auth/LoginPage.tsx";
+import RegisterPage from "../pages/auth/RegisterPage.tsx";
 
 const AppRoutes= () => {
     return(
         <Routes>
-            <Route path="/login" element={<LoginPage />} />        </Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element ={<RegisterPage />} />
+        </Routes>
+
     );
 };
 
