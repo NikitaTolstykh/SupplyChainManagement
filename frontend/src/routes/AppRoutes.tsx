20cea046116ed33ea597c4ecc6f0302546997a1b
Nikita Tolstykh <nikitatolstyh7@gmail.com>
2025-08-05 23:30:41 +0200

    feat: added routes for admin pages

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,15 +1,33 @@
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from "../pages/auth/LoginPage.tsx";
 import RegisterPage from "../pages/auth/RegisterPage.tsx";
+import AdminDashboard from "../pages/admin/AdminDashboard.tsx";
+import VehiclesPage from "../pages/admin/VehiclesPage.tsx";
+import WorkersPage from "../pages/admin/WorkersPage.tsx";
+import ProtectedRoute from "../components/ProtectedRoute";
+import { Role } from "../lib/types/Role";
 
-const AppRoutes= () => {
-    return(
+const AppRoutes = () => {
+    return (
         <Routes>
             <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element ={<RegisterPage />} />
-        </Routes>
+            <Route path="/register" element={<RegisterPage />} />
 
+            <Route
+                path="/admin"
+                element={
+                    <ProtectedRoute requiredRole={Role.ADMIN}>
+                        <AdminDashboard />
+                    </ProtectedRoute>
+                }
+            >
+                <Route index element={<WorkersPage />} />
+                <Route path="workers" element={<WorkersPage />} />
+                <Route path="vehicles" element={<VehiclesPage />} />
+            </Route>
+
+        </Routes>
     );
 };
 
-export default AppRoutes
+export default AppRoutes;
